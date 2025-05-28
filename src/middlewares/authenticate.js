7f80ded695a0963880040f7006e53071dbedbdfd
harshitcultureholidays <harshit.cultureholidays@gmail.com>
2025-05-28 08:00:34 +0530

    added authentication for fetching agent data

--- src/middlewares/authenticate.js
+++ src/middlewares/authenticate.js
@@ -1,4 +1,18 @@
 const Authenticate = {
+    authenticateDashboardUser(req, res, next){
+        try {
+            const {userid, password} = req.headers;
+            if((!userid || userid == '' || !password || password == '')){
+                return res.status(401).json('Missing credentials');
+            }
+            if(userid != process.env.dashboarduser || password != process.env.dashboardpassword){
+                return res.status(401).json('Incorrect userid and password');
+            }
+            next();
+        } catch (error) {
+            return res.status(401).json({"error" : "Unauthorized"});
+        }
+    },
     async userAuthenticate(req, res, next){
         if(!Authenticate.isUserLogin(req)){
             return res.redirect(process.env.maindomain);
--- src/routes/HomeRoute.js
+++ src/routes/HomeRoute.js
@@ -2,10 +2,11 @@ const Router = require('express').Router();
 const loginRoute = require("./LoginRoute");
 const TestRoute = require("./TestRoute");
 const DashboardRoute = require("./dashboardRoute");
+const Authenticate = require("../middlewares/authenticate")
 
 Router.use("/login", loginRoute);
 
-Router.use("/dashboard", DashboardRoute);
+Router.use("/dashboard", Authenticate.authenticateDashboardUser, DashboardRoute);
 
 Router.use("/", TestRoute);
 
